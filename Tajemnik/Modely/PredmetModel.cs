0bb16d6eb9246d42b54f3daf639f71ea370cef32
Simiae <tomas.glos@andra.cz>
2021-03-25 08:19:38 +0100

    modely

---
+++ Tajemnik/Modely/PredmetModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tajemnik.Modely
+{
+    class PredmetModel
+    {
+        public string Zkratka { get; set; }
+        public int PocetTydnu { get; set; }
+        public int HodinyPrednasek { get; set; }
+        public int HodinyCviceni { get; set; }
+        public int HodinySeminaru { get; set; }
+        public enum ZpusobZakonceni { z, zk}
+        public enum Jazyk { cz, en }
+
+
+
+    }
+}
---
+++ Tajemnik/Modely/SkupinkaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tajemnik.Modely
+{
+    public class SkupinkaModel
+    {
+        public string Zkratka { get; set; }
+        public int Rocnik { get; set; }
+        public enum Semestr { LS, ZS }
+        public int PocetStudentu { get; set; }
+        public enum FormaStudia { P, K }
+        public enum TypStudia { Bc, Mgr}
+        public enum Jazyk { Cz, En }
+
+    }
+}
